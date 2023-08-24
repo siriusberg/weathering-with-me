a413c9e88e058785e7142dbd1e0e0d2b93a7418c
Abu A'la <119801703+siriusberg@users.noreply.github.com>
2023-08-24 20:23:51 +0700

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,12 +1,12 @@
 export const geoAPIOptions = {
 	method: "GET",
 	headers: {
-	  "X-RapidAPI-Key": "YOUR API KEY FROM RAPIDAPI",// enter your rapid api key here
+	  "X-RapidAPI-Key": "YOUR API KEY FROM RAPIDAPI",
 	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
 	},
   };
   export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
   
   export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-  export const WEATHER_API_KEY = "YOUR API KEY FROM OPENWEATHERAPI"; // enter your key from openweather API
+  export const WEATHER_API_KEY = "YOUR API KEY FROM OPENWEATHERAPI";
   
